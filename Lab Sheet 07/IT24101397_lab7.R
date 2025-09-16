7eace9d0eda47cfbe9d3f68a98ec2f197c2a7349
IT24101397 <it24101397@my.sliit.lk>
2025-09-16 17:02:36 +0530

    Lab sheet 07

---
+++ Lab Sheet 07/IT24101397_lab7.R
@@ -0,0 +1,16 @@
+setwd("D:\\SLIIT JANA\\2 year\\1 st sem\\PS\Labs\\Answers\\Lab 7")
+
+#Exercise
+#Q1
+punif(25,min = 0,max = 40,lower.tail = TRUE) - punif(10,min = 0,max = 40,lower.tail = TRUE)
+
+#Q2
+pexp(2,rate = 1/3,lower.tail = TRUE)
+
+#Q3.1
+pnorm(130,mean = 100,sd = 15,lower.tail = FALSE)
+1-pnorm(130,mean = 100,sd = 15,lower.tail = TRUE)
+
+#Q3.2
+qnorm(0.95,mean = 100,sd = 15,lower.tail = TRUE)
+
